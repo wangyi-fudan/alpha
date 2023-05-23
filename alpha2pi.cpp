c5bfe6f9d124799426c815fae07c397b32c0a09e
王一 WangYi <godspeed_china@yeah.net>
2023-05-23 20:57:20 +0800

    Add files via upload

--- alpha2pi.cpp
+++ alpha2pi.cpp
@@ -2,15 +2,15 @@
 #include	<cmath>
 using	namespace	std;
 int	main(void){
-	const	long double	alpha=1/137.03599920611L;
-	long double	r=(1-alpha)/alpha,	s=0;
-	for(size_t	i=1;	i<=136;	i++)	s+=1.0L/i/i;
-	long double	pi0=sqrtl(6*s);
-	long double	pi1=sqrtl(logl(r)*2);
-	long double	pi2=sqrtl(6*(s + powl(r-136,1.0L/2/pi0)/r/r) );	//	define the intraploration of the last fractional term
+	const	long double	alpha=1/137.03599920611L;	//	Leo et al. 2020
+	long double	r=(1-alpha)/alpha,	s=0;	//	r is oddz by treat alpha as a probability
+	for(size_t	i=1;	i<=136;	i++)	s+=1.0L/i/i;	//	sum the integer terms of Basel sum
+	long double	pi0=sqrtl(6*s);	//	"Pi" estimated by integer terms of Basel sum
+	long double	pi1=sqrtl(logl(r)*2);	//	"Pi" estimated by log(r);
+	long double	pi2=sqrtl(6*(s + powl(r-136,1.0L/2/pi0)/r/r) );	//	intraploration of the last fractional term in Basel sum with pi0
 	cerr.precision(15);	
-	cerr<<"pi1\t"<<pi1<<'\n';
-	cerr<<"pi2\t"<<pi2<<'\n';
-	cerr<<"ln(pi1/pi2)\t"<<logl(pi1/pi2)<<'\n';
-	return	0;
+	cerr<<"pi1\t"<<pi1<<'\n';	//	the "Pi" estimated by log(r)
+	cerr<<"pi2\t"<<pi2<<'\n';	//	the "Pi" estimated by Basel sum
+	cerr<<"ln(pi1/pi2)\t"<<logl(pi1/pi2)<<'\n';	//	the two "Pi"s are almost equal
+	return	0;	//	Do we live in a world that Pi=3.1346195	?
 }
