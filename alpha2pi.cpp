4861a451c72e9f21ba3def12df47b4d847bc4b81
王一 WangYi <godspeed_china@yeah.net>
2023-05-23 20:37:14 +0800

    Add files via upload

--- alpha2pi.cpp
+++ alpha2pi.cpp
@@ -5,10 +5,9 @@ int	main(void){
 	const	long double	alpha=1/137.03599920611L;
 	long double	r=(1-alpha)/alpha,	s=0;
 	for(size_t	i=1;	i<=136;	i++)	s+=1.0L/i/i;
-	
+	long double	pi0=sqrtl(6*s);
 	long double	pi1=sqrtl(logl(r)*2);
-	long double	pi2=sqrtl(6*(s+powl(r-136,1.0L/2/M_PI)/r/r));
-	for(size_t	i=0;	i<100;	i++)	pi2=sqrtl(6*(s+powl(r-136,1.0L/2/pi2)/r/r));
+	long double	pi2=sqrtl(6*(s + powl(r-136,1.0L/2/pi0)/r/r) );	//	define the intraploration of the last fractional term
 	cerr.precision(15);	
 	cerr<<"pi1\t"<<pi1<<'\n';
 	cerr<<"pi2\t"<<pi2<<'\n';
